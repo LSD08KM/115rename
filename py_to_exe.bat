f30332dd715e9a4f4f6ab2180155697159635207
LSD08KM <lsd4869@hotmail.com>
2020-04-12 23:08:17 +0800

    1.0

--- py_to_exe.bat
+++ py_to_exe.bat
@@ -1,4 +1,4 @@
-pyinstaller --onefile OOF_Rename.py  --hidden-import ADC_function.py --hidden-import OOF_core.py
+pyinstaller --onefile OOF_auto_Rename.py --hidden-import fake115upload3c.py --hidden-import OOF_Rename.py  --hidden-import ADC_function.py --hidden-import OOF_core.py
 rmdir /s/q build
 rmdir /s/q __pycache__
 pause
